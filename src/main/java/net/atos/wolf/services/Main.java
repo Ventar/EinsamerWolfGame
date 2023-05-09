4a43f01098c25ea372de21a58210320bb467b9ec
Julius Reismann <julius.reismann@atos.net>
2023-05-09 10:56:11 +0200

    add arraylist

--- src/main/java/net/atos/wolf/services/Main.java
+++ src/main/java/net/atos/wolf/services/Main.java
@@ -2,10 +2,14 @@ package net.atos.wolf.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.atos.wolf.character.KaiSkill;
 
 import java.util.ArrayList;
 
 public class Main {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
 
     public static void generateSection4() throws JsonProcessingException {
         Section learn = new Section();
@@ -40,12 +44,7 @@ public class Main {
         things.add(go);
         learn.setActions(things);
 
-
-
-
-
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(learn);
+        String json = MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(learn);
         System.out.println(json);
 
     }
@@ -57,97 +56,96 @@ public class Main {
         F.setText("""
                 Du durchquerst den Wald, bis du an eine Stelle kommst, an der sich
                 der Pfad in zwei Richtungen teilt.
+                                
+                Wenn du die Kai-Disziplin Spurensuche beherrschst, lies weiter bei 96.
+                                
+                Besitzt du diese Fertigkeit nicht, kannst du entweder nach Süden
+                gehen (lies weiter bei 197) oder dich nach Osten wenden (lies
+                weiter bei 380).
                 """);
 
-        Action skill = new Action();
-        skill.setTargetSection(96);
-        skill.setMandatory(true);
-        skill.setType(ActionType.CHANGE_SECTION_IF_SKILL);
+        Action action1 = new Action();
+        action1.setTargetSection(96);
+        action1.setMandatory(true);
+        action1.setType(ActionType.CHANGE_SECTION_IF_SKILL);
+        action1.setSkill(KaiSkill.TRACKING);
 
-        Action noSkill = new Action();
-        noSkill.setTargetSection(197);
-        noSkill.setMandatory(false);
-        noSkill.setType(ActionType.CHANGE_SECTION);
+        Action action2 = new Action();
+        action2.setTargetSection(197);
+        action2.setMandatory(false);
+        action2.setType(ActionType.CHANGE_SECTION);
 
-        Action fewSkill = new Action();
-        fewSkill.setTargetSection(380);
-        fewSkill.setMandatory(false);
-        fewSkill.setType(ActionType.CHANGE_SECTION);
+        Action action3 = new Action();
+        action3.setTargetSection(380);
+        action3.setMandatory(false);
+        action3.setType(ActionType.CHANGE_SECTION);
 
-        ObjectMapper maaper = new ObjectMapper();
-        String jssohn = maaper.writerWithDefaultPrettyPrinter().writeValueAsString(F);
-        System.out.println(jssohn);
+        ArrayList<Action> thighs = new ArrayList<>();
+        thighs.add(action1);
+        thighs.add(action2);
+        thighs.add(action3);
+        F.setActions(thighs);
 
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(skill);
+
+        String json = MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(F);
         System.out.println(json);
 
-        ObjectMapper mapperr = new ObjectMapper();
-        String jsonn = mapperr.writerWithDefaultPrettyPrinter().writeValueAsString(noSkill);
-        System.out.println(jsonn);
-
-        ObjectMapper maper = new ObjectMapper();
-        String jsohn = maper.writerWithDefaultPrettyPrinter().writeValueAsString(fewSkill);
-        System.out.println(jsohn);
-
-
+//        json = MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(action1);
+//        System.out.println(json);
+//
+//        json = MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(action2);
+//        System.out.println(json);
+//
+//        json = MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(action3);
+//        System.out.println(json);
 
 
     }
 
-    public static void generateSection333() throws JsonProcessingException{
+    public static void generateSection333() throws JsonProcessingException {
 
-         Section nolearn = new Section();
-         nolearn.setSectionNumber(333);
-         nolearn.setText("""
-             Du öffnest die Tür und betrittst den Bogengang. Fünf deiner
-             Mitkadetten eilen dir mit gezogenen Waffen entgegen, entschlossen,
-             die Kammern der Kai-Meister zu verteidigen. Du trittst beiseite, um
-             sie passieren zu lassen, und während sie an dir vorbeilaufen,
-             informierst du sie noch über den Angriff, der gerade über der
-             Klosterküche stattfindet. Sie danken dir für die Warnung und ziehen
-             ihre Kapuzen über, während sie den Gang entlangeilen, den du
-             gerade gekommen bist.
-             Schließlich gelangst du zu einer Abzweigung, an der ein weiterer
-             Korridor zur Waffenkammer führt. Direkt vor dir siehst du die offene
-             Tür zu deinem Dormitorium.""");
-
-
+        Section nolearn = new Section();
+        nolearn.setSectionNumber(333);
+        nolearn.setText("""
+                Du öffnest die Tür und betrittst den Bogengang. Fünf deiner
+                Mitkadetten eilen dir mit gezogenen Waffen entgegen, entschlossen,
+                die Kammern der Kai-Meister zu verteidigen. Du trittst beiseite, um
+                sie passieren zu lassen, und während sie an dir vorbeilaufen,
+                informierst du sie noch über den Angriff, der gerade über der
+                Klosterküche stattfindet. Sie danken dir für die Warnung und ziehen
+                ihre Kapuzen über, während sie den Gang entlangeilen, den du
+                gerade gekommen bist.
+                Schließlich gelangst du zu einer Abzweigung, an der ein weiterer
+                Korridor zur Waffenkammer führt. Direkt vor dir siehst du die offene
+                Tür zu deinem Dormitorium.""");
 
 
+        Action a = new Action();
+        a.setTargetSection(192);
+        a.setMandatory(false);
+        a.setType(ActionType.CHANGE_SECTION);
 
-        Action A = new Action();
-        A.setTargetSection(192);
-        A.setMandatory(false);
-        A.setType(ActionType.CHANGE_SECTION);
+        Action b = new Action();
+        b.setTargetSection(15);
+        b.setMandatory(false);
+        b.setType(ActionType.CHANGE_SECTION);
 
-        Action B = new Action();
-        B.setTargetSection(15);
-        B.setMandatory(false);
-        B.setType(ActionType.CHANGE_SECTION);
+        ArrayList<Action> stuff = new ArrayList<>();
+        stuff.add(a);
+        stuff.add(b);
+        nolearn.setActions(stuff);
 
 
-        ObjectMapper mapperr = new ObjectMapper();
-        String jsonn = mapperr.writerWithDefaultPrettyPrinter().writeValueAsString(nolearn);
-        System.out.println(jsonn);
 
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(A);
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(nolearn);
         System.out.println(json);
-
-        ObjectMapper maper = new ObjectMapper();
-        String jsohn = maper.writerWithDefaultPrettyPrinter().writeValueAsString(B);
-        System.out.println(jsohn);
-
-
-
-
 
 
     }
 
     public static void main(String[] args) throws JsonProcessingException {
-        generateSection4();
+        //generateSection4();
         generateSection332();
         generateSection333();
 
