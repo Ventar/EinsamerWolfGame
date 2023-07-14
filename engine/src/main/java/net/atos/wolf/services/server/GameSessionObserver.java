d90754bc0bfd5468cb8c6602de6dd8663c6c884e
Leon Federau <leon.federau@atos.net>
2023-07-14 11:08:47 +0200

    add session expiration time

--- engine/src/main/java/net/atos/wolf/services/server/GameSessionObserver.java
+++ engine/src/main/java/net/atos/wolf/services/server/GameSessionObserver.java
@@ -1,36 +1,38 @@
 package net.atos.wolf.services.server;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import net.atos.wolf.services.GameSession;
 import net.atos.wolf.services.SessionService;
 
 import java.time.LocalDate;
+
 @Slf4j
-public class GameSessionObserver extends Thread{
+public class GameSessionObserver extends Thread {
 
     private SessionService sessionService;
 
-    private static long threadSleepDuration;
+    private static long threadSleepDuration = 10000;
 
-    private static long sessionExpireTime;
+    private static long ExpireTime = 35000;
 
     public GameSessionObserver(SessionService service) {
         this.sessionService = service;
     }
 
-
     public void run() {
-        LOG.debug("CREATION DATE");
         while (true) {
             try {
-                Thread.sleep(3000);
-//                for (int i = 0; i < sessionService.serviceHashMap().size(); i++) {
-//
-//                    LocalDate localDate = LocalDate.now().minusDays(sessionService.serviceHashMap().get(i).creationDate().toEpochDay());
-//
-//                    System.out.println(localDate);
-//                }
+                Thread.sleep(threadSleepDuration);
+                sessionService.serviceHashMap().forEach((key, value) -> {
+                    long sessionExpireTime = System.currentTimeMillis() - value.lastUsed();
+                    System.out.println(sessionExpireTime);
+                    if (sessionExpireTime > ExpireTime) {
+                        sessionService.serviceHashMap().remove(key, value);
+                    }
+                });
             } catch (Exception e) {
                 LOG.debug("exception occurred", e);
             }
