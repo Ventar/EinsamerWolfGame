b6ba45988877cfe8ff387be636c47eb2af761cce
Noel Masur <noel.masur@atos.net>
2023-10-02 08:58:40 +0200

    Start work on drop weapon servlet

---
+++ engine/src/main/java/net/atos/wolf/server/servlet/WeaponDropServlet.java
@@ -0,0 +1,75 @@
+package net.atos.wolf.server.servlet;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import net.atos.wolf.data.Action;
+import net.atos.wolf.data.Character;
+import net.atos.wolf.data.GameSession;
+import net.atos.wolf.data.Item;
+import net.atos.wolf.data.Weapon;
+import net.atos.wolf.server.GameServlet;
+import net.atos.wolf.service.gameengine.ActionType;
+
+@Slf4j
+@GameServlet("/weapon/drop/")
+public class WeaponDropServlet extends BaseServlet{
+
+    @ToString
+    public static class RequestData {
+
+        public String id;
+
+        public int Weapon;
+
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            WeaponDropServlet.RequestData data =registry.jsonMapper().readValue(request.getReader(), WeaponDropServlet.RequestData.class);
+            LOG.debug("Received game servlet request with data: {}", data);
+            GameSession session = registry.sessionService().getSessionById(data.id);
+            LOG.debug("Current game session: {}", session);
+
+            if (session != null) {
+
+                if (data.Weapon == 1){
+                    session.character().weaponOne(null);
+                }
+
+                if (data.Weapon == 2){
+                    session.character().weaponTwo(null);
+                }
+
+
+
+
+//                Action pickUpAction = new Action();
+//                pickUpAction.type(ActionType.TAKE_ITEM);
+//                pickUpAction.text("Hebe den Gegenstand wieder auf (" + registry.translation().translate(weapon1.id()).de()  + ")");
+//                pickUpAction.weapon(weapon1);
+//
+//                session.modifiedAnswerOptions().add(0,pickUpAction);
+
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println(registry.jsonMapper().writeValueAsString(session));
+            } else {
+                response.setContentType("text/plain");
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+
+        } catch (Exception e) {
+            LOG.debug("Exception during post to /section :", e);
+            response.setContentType("text/plain");
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+}
