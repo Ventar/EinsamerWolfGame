4a2d0f87c756591c52b22238f7fb735cad2f7293
Michael Rodenbuecher <michael.rodenbuecher@atos.net>
2023-10-02 08:32:41 +0200

    Work on UI components

---
+++ ui/src/routes/Character/AttributeCard.svelte
@@ -0,0 +1,83 @@
+<script>
+  import IconCard from "./IconCard.svelte";
+
+  /**
+   * @type String
+   */
+  export let image;
+
+  /**
+   * @type String
+   */
+  export let name;
+
+  /**
+   * @type number
+   */
+  export let currentValue;
+
+  /**
+   * @type number
+   */
+  export let maxValue;
+
+  /**
+   * @param {number} max
+   */
+  function totalAttribute(max) {
+    return { length: 50 - max };
+  }
+
+  /**
+   * @param {number} current
+   * @param {number} max
+   */
+  function lostAttribute(current, max) {
+    return { length: max - current };
+  }
+
+  /**
+   * @param {number} current
+   */
+  function currentAttribute(current) {
+    return { length: current };
+  }
+</script>
+
+<IconCard caption={name} src={image} cols={3}>
+  <div class="card-body flexwrap">
+    {#each currentAttribute(currentValue) as _}
+      <div class="rcorners1" />
+    {/each}
+    {#each lostAttribute(currentValue, maxValue) as _}
+      <div class="rcorners2" />
+    {/each}
+    {#each totalAttribute(maxValue) as _}
+      <div class="rcorners3" />
+    {/each}
+  </div>
+</IconCard>
+
+<style>
+  .rcorners1 {
+    border-radius: 2px;
+    background: #73ad21;
+    margin: 2px;
+    width: 15px;
+    height: 15px;
+  }
+  .rcorners2 {
+    border-radius: 2px;
+    background: #555555;
+    margin: 2px;
+    width: 15px;
+    height: 15px;
+  }
+  .rcorners3 {
+    border-radius: 2px;
+    background: #303030;
+    margin: 2px;
+    width: 15px;
+    height: 15px;
+  }
+</style>
---
+++ ui/src/routes/Character/IconCard.svelte
@@ -0,0 +1,36 @@
+<script>
+  // ---------------------------------------------------------------------------------------
+  // Input parameter
+  // ---------------------------------------------------------------------------------------
+
+  /**
+   * The caption of the card
+   * @type String
+   */
+  export let caption;
+
+  /**
+   * The content of the src attribute of the image tag for the header
+   * @type String
+   */
+  export let src;
+
+  /**
+   * Nzmber of colums to cover.
+   * @type Number
+   */
+  export let cols = 3;
+</script>
+
+<div class="col-lg-{cols}" style="display: flex; flex-grow: 1;">
+  <div class="bs-component flexwrap">
+    <div class="card mb-3 flexwrap">
+      <div class="card-header">
+        <img height="40" width="40" {src} alt={caption} />
+        <span style="margin-left: 15px; font-weight: bold; align-items: center;">{caption}</span>
+      </div>
+
+      <slot />
+    </div>
+  </div>
+</div>
